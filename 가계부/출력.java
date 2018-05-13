9b19b98f62e6bbdddb13eea152350f4764f22571
geumkyeong <geumkyeong@github.com>
2018-05-14 00:19:07 +0900

    5차과제 수정

--- 가계부/출력.java
+++ 가계부/출력.java
@@ -1,11 +1,14 @@
 package 가계부;
 
 public class 출력 {
-	private String[] 결산;
+	private String[] 결산항목, 결산품목;
+	private int[] 결산액;
 	private int 총수입, 총지출, 총잔액;
 	
-	public void 출력자료_설정(String[] 결산, int 총수입, int 총지출, int 총잔액) {
-		this.결산 = 결산;
+	public void 출력자료_설정(String[] 결산항목, String[] 결산품목, int[] 결산액, int 총수입, int 총지출, int 총잔액) {
+		this.결산항목 = 결산항목;
+		this.결산품목 = 결산품목;
+		this.결산액 = 결산액;
 		this.총수입 = 총수입;
 		this.총지출 = 총지출;
 		this.총잔액 = 총잔액;
@@ -13,12 +16,10 @@ public class 출력 {
 
 	public void 결산장부() {
 		System.out.println("========== 장부내역  ==========");
-		//결산장부 출력
-		for(int i = 0; i < 결산.length; i += 3) {
-			if( 결산[i] != null ) 
-				System.out.println( 결산[i] + " > " + 결산[i+1] + " : " + 결산[i+2] );
-			else
-				break;
+		for(int i = 0; i < 10; i++) {
+			if(결산품목[i] != null) {
+				System.out.println( 결산항목[i] + " > " + 결산품목[i] + " : " + 결산액[i]);
+			}else { break; }
 		}
 		System.out.println(" ");
 		//총결산액 출력

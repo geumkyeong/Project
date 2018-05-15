8df5410dcbf8e9c3198fc4c173f4625f115d8d91
geumkyeong <geumkyeong@github.com>
2018-05-16 02:24:39 +0900

    modified Project 6

--- 가계부/장부.java
+++ 가계부/장부.java
@@ -1,8 +1,33 @@
 package 가계부;
 public class 장부 {
+	private String[] 항목들, 품목들;
+	private int[] 금액들, 누적잔액들, 수입차액들, 지출차액들, 수입비율들, 지출비율들;
 	private String 수입품목, 지출품목;
-	private int 수입금액, 지출금액, 누적수입, 누적지출, 누적잔액, 수입차액, 지출차액, 수입비율, 지출비율;
+	private int 항목수, 수입금액, 지출금액, 누적수입, 누적지출, 누적잔액, 수입차액, 지출차액, 수입비율, 지출비율;
 	
+	장부() {
+		항목들 = new String[10];
+		품목들 = new String[10];
+		금액들 = new int[10];
+		누적잔액들 = new int[10];
+		수입차액들 = new int[10];
+		지출차액들 = new int[10];
+		수입비율들 = new int[10];
+		지출비율들 = new int[10];
+	}
+	public void 수지내역_설정(String 항목, String 품목, int 금액, int 누적잔액, int 수입차액, int 지출차액, int 수입비율, int 지출비율) {
+		if ( 항목수 < 항목들.length ) {
+			항목들[항목수] = 항목; 
+			품목들[항목수] = 품목;
+			금액들[항목수] = 금액;
+			누적잔액들[항목수] = 누적잔액;
+			수입차액들[항목수] = 수입차액;
+			지출차액들[항목수] = 지출차액;
+			수입비율들[항목수] = 수입비율;
+			지출비율들[항목수] = 지출비율;
+		} //항목수가 배열길이를 넘지 않는 한에서 삽입
+			항목수++;
+	}
 	public void 장부_설정(String 항목, String 품목, int 금액) {
 		if( 항목.equals("수입") ) { 
 			수입품목 = 품목;
@@ -20,15 +45,22 @@ public class 장부 {
 		}
 		누적잔액 = 누적수입 - 누적지출;
 	}
-	public void 차액_관리(String 항목, int 수입예산, int 지출예산) {
-		if( 항목.equals("수입") ) { 
+	public void 차액_관리(int 수입예산, int 누적수입, int 지출예산, int 누적지출) {
 			수입차액 = 수입예산 - 누적수입;
 			수입비율 = (int)((double)수입차액 / (double)수입예산 * 100);
-		} else if( 항목.equals("지출") ) {
 			지출차액 = 지출예산 - 누적지출;
 			지출비율 = (int)((double)지출차액 / (double)지출예산 * 100);
-		}
 	}
+	
+	public String[] 항목들_반환() { return 항목들; }
+	public String[] 품목들_반환() { return 품목들; }
+	public int[] 금액들_반환() { return 금액들; }
+	
+	public int[] 누적잔액들_반환() { return 누적잔액들; }
+	public int[] 수입차액들_반환() { return 수입차액들; }
+	public int[] 지출차액들_반환() { return 지출차액들; }
+	public int[] 수입비율들_반환() { return 수입비율들; }
+	public int[] 지출비율들_반환() { return 지출비율들; }
 	
 	public String 수입품목_반환() { return 수입품목; }
 	public int 수입금액_반환() { return 수입금액; }
--- 가계부/출력.java
+++ 가계부/출력.java
@@ -1,22 +1,13 @@
 package 가계부;
 
 public class 출력 {
-	public void 수지내역_출력(String 항목, String 품목, int 금액) {
-		System.out.println("========== 수지내역  ==========");
-		System.out.println(항목 + " > " + 품목 + " " + 금액);
-	}
-	public void 누적내역_출력(int 누적수입, int 누적지출, int 누적잔액) {
-		System.out.println("========== 누적내역  ==========");
-		System.out.println("누적수입  > " + 누적수입);
-		System.out.println("누적지출  > " + 누적지출);
-		System.out.println("누적잔액  > " + 누적잔액);
-	}
-	public void 예산차액_비율_출력(int 수입차액, int 지출차액, double 수입비율, double 지출비율) {
-		System.out.println("========== 예산대비차액 ==========");
-		System.out.println("수입차액  > " + 수입차액);
-		System.out.println("지출차액  > " + 지출차액);
-		System.out.println("========== 예산대비비율 ==========");
-		System.out.println("수입비율  > " + 수입비율 + "%");
-		System.out.println("지출비율  > " + 지출비율 + "%");
+	public void 수지내역_출력(String[] 항목들, String[] 품목들, int[] 금액들, int[] 누적잔액들, int[] 수입차액들, int[] 지출차액들, int[] 수입비율들, int[] 지출비율들) {
+		System.out.println("내역		금액	수입차액	지출차액");
+		for( int i = 0; i < 항목들.length; i ++ ) {
+			if(항목들[i] != null) {
+				System.out.println(항목들[i] + " > " + 품목들[i] + " : " + 금액들[i] + " " + 누적잔액들[i] +
+						" " + 수입차액들[i] + " " + 수입비율들[i] + "% " + 지출차액들[i] + " " + 지출비율들[i] +"% ");
+			}
+		}
 	}
 }
